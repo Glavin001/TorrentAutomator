8ee7d34885c251db2df1bcd4d1dee72b87914924
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-08-16 15:04:25 -0300

    Closes #1. Kickass Torrents Service.

---
+++ server/kickassService.js
@@ -0,0 +1,34 @@
+var Kickass = require('node-kickass-json');
+
+module.exports = {
+    kickass: null,
+
+    setup: function(app) {
+        // Setup
+        console.log('Setup Kickass');
+        this.kickass = new Kickass();
+    },
+
+    find: function(params, callback) {
+        console.log(params);
+        var query = params.query;
+        this.kickass.setQuery(query.query) // Set search Query parameter
+        .setSort({
+            field: query.field || 'seeders',
+            sorder: query.sorder || 'desc'
+        }).run(function(error, data) {
+            return callback(error, data);
+        });
+    },
+
+    // get: function(id, params, callback) {
+    //   this.transmission.get(id, function(err, result) {
+    //       if (err) {
+    //           return callback(err, result);
+    //       }
+    //       console.log('bt.get returned ' + result.torrents.length + ' torrents');
+    //       return callback(result.torrents);
+    //   });
+    // }
+
+};
