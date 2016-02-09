d565bb96a038c842bc5bb203409771dc4215bd97
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-02-09 15:39:14 -0400

    Fix permissinos of mounted shared network drive

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Mount network shared drive
 mkdir -p /mnt/Public
-mount -t cifs -o username=root,password= //192.168.1.123/Public /mnt/Public
+mount -t cifs -o username=root,password=,guest,uid=1000,gid=1000,rw,file_mode=0777,dir_mode=0777,sfu //192.168.1.123/Public /mnt/Public
 
 # Start transmission!
 service transmission-daemon reload
