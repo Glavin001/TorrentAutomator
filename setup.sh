60094ac0f94201227ec6069c4cd966dc2a936c9b
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-02-09 00:05:31 -0400

    Clean npm cache after install

--- setup.sh
+++ setup.sh
@@ -11,6 +11,10 @@ apt-get install nfs-common
 apt-get install cifs-utils
 apt-get install transmission-daemon
 
+# Clean cache
+npm cache clean
+rm -rf /tmp/*
+
 echo "Mount network shared drive"
 mount -t cifs -o username=root,password= //192.168.1.123/Public /mnt/Public
 echo "Configure Transmission"
